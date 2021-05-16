8b22f5909c35103e917cb7822955abfa38a10d0e
cmerlo <caromerlosan@gmail.com>
2021-05-15 21:09:58 -0400

    Ultimas modificaciones

--- CaroMerlo-Prueba1/assets/js/main.js
+++ CaroMerlo-Prueba1/assets/js/main.js
@@ -11,13 +11,14 @@ $(document).ready(function () {
 });
 
 
+/*Evento. Mover el cursor sobre el elemento. cambia de color a naranjo*/
 window.addEventListener('mouseover', function(evt) {  
     if (evt.target.classList.contains('navOrange')) {
       evt.target.style.color = 'orange';
     }
   });
 
-  
+/*Evento. Retira el cursor sobre el elemento. cambia de color a blanco*/
 window.addEventListener('mouseout', function(evt) {
     if (evt.target.classList.contains('navOrange')) {
       evt.target.style.color = 'white';
