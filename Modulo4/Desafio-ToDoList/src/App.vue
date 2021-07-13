29ceea41764099067109cf3eb0960bc27ab885aa
cmerlo <caromerlosan@gmail.com>
2021-07-13 18:32:44 -0400

    Desafio VueJs2

---
+++ Modulo4/Desafio-ToDoList/src/App.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <div id="app">    
+    <div v-if="mostrar">
+      {{ message }}
+      <input v-model="new_task"  />
+      <button @click="createTask">Crear</button>          
+      <ul>
+        <li v-for="tarea in tareas">
+          <span>{{ tarea.name }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import lista from "./lista.vue";
+
+export default {
+  name: "app",
+  components: {
+    lista
+  },
+  data() {
+    return {      
+      tareas: [{ name: "Hacer Desafio VueJs" }, { name: "Ir al supermercado" }],
+      mostrar: true,
+      message: "Tarea",
+      
+    };
+    
+  },
+  
+  methods: {    
+    createTask: function () {
+    this.tareas.push({
+        name: this.new_task,
+       
+    });
+
+    this.new_task = '';
+}
+  }
+};
+</script>
---
+++ Modulo4/Desafio-ToDoList/src/lista.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="lista">
+    soy un primer texto estatico de un componente hijo
+    {{ message1 }}
+    <button @click="clickResponse">Llamar al padre</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "lista",
+  props: {
+    message1: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    clickResponse(event) {
+      //sentencias, condicion, iteracion x cada, emito un evento ,etc
+      this.$emit("eventoDelHijo", "Soy info de un hijo");
+    }
+  }
+
+  
+};
