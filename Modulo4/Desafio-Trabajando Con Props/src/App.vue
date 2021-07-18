19279677f68997a5a8bed66b7d1cda0c454037cc
cmerlo <caromerlosan@gmail.com>
2021-07-17 22:38:54 -0400

    nuevo deafio

---
+++ Modulo4/Desafio-Trabajando Con Props/src/App.vue
@@ -0,0 +1,62 @@
+<template lang="html">
+  <div id="app">    
+    <div v-if="mostrar">
+      {{ message }}
+      <input v-model="new_task"  />
+      <button @click="creaTask">Crear</button>         
+      <ul>
+        <li v-for="tarea in tareas">
+          <span>{{ tarea.name }}</span>
+          <button @click="eliminarTask(index)">Eliminar</button>  
+          
+        </li>
+      </ul>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import HelloVue from "./helloVue.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloVue
+  },
+  data() {
+    return {      
+      tareas: [{ name: "Hacer Desafio VueJs" }, { name: "Ir al supermercado" }],
+      mostrar: true,
+      message: "Tarea",      
+    };    
+  },
+  computed: {   
+  },
+  
+  methods: {    
+    creaTask() {
+       this.tareas.push({
+        name: this.new_task,       
+    });
+    this.new_task = '';
+    },
+    eliminarTask(index) {
+      this.tareas.splice(index, 1);
+    },
+
+      metodoDelPadre() {
+      console.log("Evento del hijo controlado");
+      this.checked = !this.checked;
+      this.frutas.push({ name: "NuevaFruta", comprado: true });
+    },
+    mostrarComponente() {
+      console.log("Soy un metodo");
+      this.mostrar = !this.mostrar;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Modulo4/Desafio-Trabajando Con Props/src/helloVue.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hellovue">
+   
+    {{ message1 }}
+    <button @click="clickGuardar">Llamar al padre</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloVue",
+  props: {
+    message1: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {    
+    clickGuardar() {
+        this.$emit('creaTask', sasdad);
+     },
+  }
+};
+</script>
+
+<style scoped>
+.hellovue {
+  font-size: 20px;
+  border: 1px solid black;
+  padding: 20px;
+}
+</style>
